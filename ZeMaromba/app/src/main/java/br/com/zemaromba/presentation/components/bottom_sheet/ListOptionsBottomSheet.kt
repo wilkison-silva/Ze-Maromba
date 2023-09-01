866d64cb25f285ad3c2266101b95b8cfc93d0c62
Wilkison Silva <wilkisonmartinsdasilva@gmail.com>
2023-09-01 16:08:37 -0400

    feat: ajustes de layout no bottom sheet de opcoes

--- ZeMaromba/app/src/main/java/br/com/zemaromba/presentation/components/bottom_sheet/ListOptionsBottomSheet.kt
+++ ZeMaromba/app/src/main/java/br/com/zemaromba/presentation/components/bottom_sheet/ListOptionsBottomSheet.kt
@@ -1,16 +1,12 @@
 package br.com.zemaromba.presentation.components.bottom_sheet
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -18,9 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import br.com.zemaromba.presentation.core_ui.ui.theme.Dimens
 import br.com.zemaromba.presentation.core_ui.ui.theme.Styles
@@ -58,7 +52,7 @@ fun ListOptionsBottomSheet(
                 ),
             text = title,
             textAlign = TextAlign.Left,
-            style = Styles.Title4Normal
+            style = Styles.Title3Normal
         )
         Divider(
             modifier = Modifier.fillMaxWidth(),
@@ -66,11 +60,7 @@ fun ListOptionsBottomSheet(
         )
         LazyColumn(
             modifier = Modifier
-                .padding(
-                    start = Dimens.Space.space_20dp,
-                    end = Dimens.Space.space_20dp,
-                    bottom = Dimens.Space.space_64dp
-                )
+                .padding(bottom = Dimens.Space.space_64dp)
                 .fillMaxWidth(),
         ) {
             itemsIndexed(
@@ -94,7 +84,7 @@ fun ListOptionsBottomSheet(
                             .padding(
                                 top = Dimens.Space.space_20dp,
                                 bottom = Dimens.Space.space_20dp,
-                                start = Dimens.Space.space_8dp
+                                start = Dimens.Space.space_28dp
                             ),
                         text = textButtonName,
                         textAlign = TextAlign.Left,

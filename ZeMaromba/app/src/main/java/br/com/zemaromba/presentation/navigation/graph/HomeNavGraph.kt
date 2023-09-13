f0a0c736c4529417437bcc7217579601379f07f6
Wilkison Martins da Silva <74081311+wilkison-silva@users.noreply.github.com>
2023-09-13 15:19:14 -0400

    Added: Funcionalidade de trocar nome do usuário(#14)

--- ZeMaromba/app/src/main/java/br/com/zemaromba/presentation/navigation/graph/HomeNavGraph.kt
+++ ZeMaromba/app/src/main/java/br/com/zemaromba/presentation/navigation/graph/HomeNavGraph.kt
@@ -2,16 +2,21 @@ package br.com.zemaromba.presentation.navigation.graph
 
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.navigation
 import br.com.zemaromba.common.extensions.composableWithTransitionAnimation
 import br.com.zemaromba.presentation.navigation.router.HomeRouter
 import br.com.zemaromba.presentation.model.MenuHome
 import br.com.zemaromba.presentation.home.screen.HomeScreen
+import br.com.zemaromba.presentation.home.screen.UserManagementScreen
 import br.com.zemaromba.presentation.home.viewmodel.HomeScreenViewModel
+import br.com.zemaromba.presentation.home.viewmodel.UserManagementViewModel
+import br.com.zemaromba.presentation.navigation.router.ExerciseRouter
+import br.com.zemaromba.presentation.navigation.router.TrainingPlanRouter
 
 fun NavGraphBuilder.homeGraph(
-    navigateTo: (menu: MenuHome) -> Unit
+    navController: NavController
 ) {
     navigation(
         startDestination = HomeRouter.HomeScreen.route,
@@ -26,8 +31,40 @@ fun NavGraphBuilder.homeGraph(
             HomeScreen(
                 state = state,
                 onNavigate = {
-                    navigateTo(it)
+                    when (it) {
+                        MenuHome.TRAINING_PLAN_SCREEN -> {
+                            navController.navigate(
+                                route = TrainingPlanRouter.TrainingPlanGraph.route
+                            )
+                        }
+
+                        MenuHome.EXERCISES_SCREEN -> {
+                            navController.navigate(ExerciseRouter.ExerciseGraph.route)
+                        }
+                    }
+                },
+                onIconAccountSettingClick = {
+                    navController.navigate(HomeRouter.UserManagementScreen.route)
                 }
+            )
+        }
+        composableWithTransitionAnimation(
+            route = HomeRouter.UserManagementScreen.route,
+        ) {
+            val viewModel: UserManagementViewModel = hiltViewModel()
+            val state = viewModel.state.collectAsStateWithLifecycle().value
+
+            UserManagementScreen(
+                state = state,
+                onChangeName = {
+                    viewModel.onEnterNewName(name = it)
+                },
+                onSaveName = {
+                    viewModel.onSaveName()
+                },
+                onNavigateBack = {
+                    navController.popBackStack()
+                },
             )
         }
     }

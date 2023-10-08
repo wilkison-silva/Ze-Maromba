9841bb193a3970ede4bd18415114036f0b01c633
Wilkison Silva <wilkisonmartinsdasilva@gmail.com>
2023-10-08 12:38:47 -0400

    feat: altera versao para 1.0.2

--- ZeMaromba/buildSrc/src/main/java/Dependencies.kt
+++ ZeMaromba/buildSrc/src/main/java/Dependencies.kt
@@ -6,8 +6,8 @@ object Dependencies {
         const val applicationId = "br.com.zemaromba"
         const val minSdk = 24
         const val targetSdk = 34
-        const val versionCode = 3
-        const val versionName = "1.0.1"
+        const val versionCode = 4
+        const val versionName = "1.0.2"
         const val kotlinCompilerExtensionVersion = "1.5.0"
     }
 
